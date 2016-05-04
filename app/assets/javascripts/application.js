9e8cbd407414eaa0f97bcb0caee0132debfaeced
Terry Roberts <s3584197@student.rmit.edu.au>
2016-05-04 19:59:17 +1000

    Edited CSS files to use Boostrap/SASS
    Fixed bootstrap loaders and started editing CSS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-datepicker
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require bootstrap-datepicker
 //= require_tree .
